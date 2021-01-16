2c5851e72a12ca67ebd68154825f5362dc61cf57
VasiliB <73487624+VasiliB@users.noreply.github.com>
2021-01-16 21:01:29 +0300

    Fixed

--- app/src/main/java/ru/anyname/myapplication/FragmentMoviesList.kt
+++ app/src/main/java/ru/anyname/myapplication/FragmentMoviesList.kt
@@ -13,23 +13,48 @@ import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.fragment.findNavController
 import kotlinx.android.synthetic.main.fragment_movies_details.*
 import kotlinx.android.synthetic.main.fragment_movies_list.*
+import ru.anyname.myapplication.databinding.FragmentMoviesListBinding
 
-class FragmentMoviesList : Fragment() {
 
-    override fun onCreateView(
-            inflater: LayoutInflater, container: ViewGroup?,
-            savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_movies_list, container, false)
+//class FragmentMoviesList : Fragment() {
+//
+//    override fun onCreateView(
+//            inflater: LayoutInflater, container: ViewGroup?,
+//            savedInstanceState: Bundle?
+//    ): View? {
+//        // Inflate the layout for this fragment
+//        return inflater.inflate(R.layout.fragment_movies_list, container, false)
+//
+//    }
+//
+//    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+//        super.onViewCreated(view, savedInstanceState)
+//
+//        bg_movie.setOnClickListener {
+//            findNavController().navigate(R.id.action_FragmentMoviesList_to_FragmentMoviesDetails)
+//        }
+//    }
+//}
 
-    }
+class FragmentMoviesList : Fragment(R.layout.fragment_movies_list) {
+    // Scoped to the lifecycle of the fragment's view (between onCreateView and onDestroyView)
+    private var fragmentMovieslistBinding: FragmentMoviesListBinding? = null
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        fragmentMovieslistBinding = FragmentMoviesListBinding.bind(view)
 
-        bg_movie.setOnClickListener {
+        fragmentMovieslistBinding?.button.apply{
+            this?.click
+        } {
             findNavController().navigate(R.id.action_FragmentMoviesList_to_FragmentMoviesDetails)
         }
+    }
+
+    override fun onDestroyView() {
+        // Consider not storing the binding instance in a field
+        // if not needed.
+        fragmentMovieslistBinding = null
+        super.onDestroyView()
     }
 }
